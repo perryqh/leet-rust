14b0a32a54fa54f3503552a6c8bfd6d55741fd4c
Perry Hertler <perry.hertler@gusto.com>
2023-11-26 21:23:03 -0600

    refactoring for clarity

--- sort-vowels/src/lib.rs
+++ sort-vowels/src/lib.rs
@@ -4,23 +4,29 @@ impl Solution {
     const VOWELS: [char; 10] = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
     pub fn sort_vowels(s: String) -> String {
-        let mut vowels: Vec<char> = vec![];
-        let mut vowel_indexes: Vec<usize> = vec![];
-        let mut result: Vec<char> = vec![];
+        let mut result = String::with_capacity(s.len());
 
-        for (index, current_char) in s.chars().enumerate() {
-            result.push(current_char);
-            if Solution::VOWELS.contains(&current_char) {
-                vowels.push(current_char);
-                vowel_indexes.push(index);
+        let mut reverse_vowels = Solution::reverse_sorted_vowels(&s);
+
+        for c in s.chars() {
+            if Solution::VOWELS.contains(&c) {
+                result.push(reverse_vowels.pop().unwrap());
+            } else {
+                result.push(c);
             }
         }
-        vowels.sort_unstable();
-        vowels.reverse();
-        for index in vowel_indexes {
-            result[index] = vowels.pop().unwrap();
-        }
-        String::from_iter(result)
+
+        result
+    }
+
+    fn reverse_sorted_vowels(s: &str) -> Vec<char> {
+        let mut all_vowels: Vec<_> = s
+            .chars()
+            .filter(|c| Solution::VOWELS.contains(c))
+            .collect();
+        all_vowels.sort_unstable();
+        all_vowels.reverse();
+        all_vowels
     }
 }
 
